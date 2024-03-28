c8244b9d5254c90cc1fad768550455b50cdd62fd
Humberto Caetano <humberto.ccs@gmail.com>
2024-03-28 08:41:25 -0300

    Nova Branch

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ app.get('/', function (req, res) {
    res.send('Hello Geeks!');
    res.send('<br>');
    res.send('Nova Linha!');
+   res.send('<br>');
+   res.send('Nova Branch!');
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
